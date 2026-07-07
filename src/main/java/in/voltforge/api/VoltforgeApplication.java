1da3f82849241f0312102681474485771ee22eec
Prasad <v.sivaprasad7482@gmail.com>
2026-07-07 12:14:10 +0530

    Deployment

--- src/main/java/in/voltforge/api/VoltforgeApplication.java
+++ src/main/java/in/voltforge/api/VoltforgeApplication.java
@@ -2,10 +2,14 @@ package in.voltforge.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableJpaRepositories(basePackages = "in.voltforge.api")
+@EntityScan(basePackages = "in.voltforge.api")
 public class VoltforgeApplication {
 
     public static void main(String[] args) {
